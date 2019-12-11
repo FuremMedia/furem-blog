d557ff8ec3a27f0f87427921027e061f39fc740e
Josh Brigden <joshuabrigden2@gmail.com>
2019-12-11 17:11:37 +0000

    Blog Update

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -10,7 +10,7 @@ import theCMS from '../cms/netlify/cms'
  */
 export default {
   siteName: 'Furem',
-  tagline: 'A Nuxt.js Theme for Netlify CMS',
+  tagline: 'Our Official Newsroom and Blog',
   featureImage: '/uploads/home-hero.jpg',
   logo: 'logo-component', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
@@ -19,27 +19,10 @@ export default {
   },
   mainMenu: [
     {
-      name: 'About',
-      link: '/about'
+      name: 'Home',
+      link: 'https://www.furemmedia.co.uk'
     },
-    {
-      name: 'Contact',
-      link: '/contact'
-    },
-    {
-      name: 'Categories',
-      link: '/categories'
-    },
-    {
-      name: 'Github Repo',
-      link: 'https://github.com/danielkellyio/awake-template',
-      target: '_blank'
-    },
-    {
-      name: 'Awake+ Premium',
-      link: 'http://awake-premium-lp.danielkelly.io/',
-      target: '_blank'
-    }
+    
   ],
   hero: {
     theme: 'dark' // options: mist, light, dark
@@ -61,25 +44,7 @@ export default {
   disqus: {
     on: true,
     loadingStrategy: 'button', // Options: onload, lazy, button
-    siteShortName: 'blog-danielkelly-io' // 'blog-danielkelly-io'
-  },
-
-  // Newsletter Subscribe
-  newsletter: {
-    on: true,
-    heading: 'Subscribe to Our Newsletter',
-    btnText: 'Subscribe',
-    // Can be the form action on a mail chimp form, a hubspot form,
-    // or any other url you want to post the form data to
-    mailchimp: {
-      on: true,
-      formAction:
-        'https://danielkelly.us3.list-manage.com/subscribe/post?u=d3c2e762b66a2353d0bc29bf3&amp;id=2abeaa06e0'
-    },
-    custom: {
-      on: false,
-      formAction: ''
-    }
+    siteShortName: 'furemmedia' // 'blog-danielkelly-io'
   },
 
   // Categories
